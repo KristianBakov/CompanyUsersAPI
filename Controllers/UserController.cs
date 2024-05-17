6484149c7fcd58794771561fd519ab6df821874a
KristianBakov <kristian.bakov@yahoo.com>
2024-05-17 16:26:38 +0100

    Added data context dapper

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -6,10 +6,18 @@ namespace CompanyUsersAPI.Controllers
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
+        DataContextDapper _dapper;
         public UserController(IConfiguration config)
         {
-
+            _dapper = new DataContextDapper(config);
         }
+
+        [HttpGet("TestConnection")]
+        public DateTime TestConnection()
+        {
+            return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+        }
+
 
         [HttpGet("GetUsers/{testValue}")]
         public string[] GetUsers(string testValue)
