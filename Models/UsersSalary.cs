19b82361aa734471179741b1910cf5b044f627c9
KristianBakov <kristian.bakov@yahoo.com>
2024-05-17 16:41:29 +0100

    Set up user models

---
+++ Models/UsersSalary.cs
@@ -0,0 +1,9 @@
+namespace CompanyUsersAPI.Models
+{
+    public partial class UserSalary
+    {
+        public int UserId { get; set; }
+        public decimal Salary { get; set; }
+        //public decimal AvgSalary { get; set; }
+    }
+}
